b66ffffe9ced9008b8a5704eeac40bc604a77bdb
Florentin Picioroaga <Florentin.Picioroaga@harman.com>
2012-06-01 18:25:46 +0200

    upload some local files from the previous commit.

---
+++ plugins/org.franca.deploymodel.dsl/src/org/franca/deploymodel/dsl/DeploymentContainer.java
@@ -0,0 +1,8 @@
+package org.franca.deploymodel.dsl;
+
+import java.util.ArrayList;
+
+import org.franca.core.framework.IModelContainer;
+
+public class DeploymentContainer extends ArrayList<PersistentModel> implements IModelContainer {
+}
