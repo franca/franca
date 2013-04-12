2cab1647256dc9d4c1d5ba2ed27b155390a9d403
Klaus Birken <klaus.birken@gmail.com>
2013-04-12 17:51:26 +0200

    Provide UI action to convert DBus Introspection xml files to Franca IDL files. Currently, this action is available for any kind of xml file. This will be restricted in future.

--- plugins/org.franca.connectors.dbus.ui/src/org/franca/connectors/dbus/ui/actions/CreateFrancaFromDBusXMLAction.java
+++ plugins/org.franca.connectors.dbus.ui/src/org/franca/connectors/dbus/ui/actions/CreateFrancaFromDBusXMLAction.java
@@ -7,8 +7,6 @@
  *******************************************************************************/
 package org.franca.connectors.dbus.ui.actions;
 
-import java.util.Collection;
-
 import model.emf.dbusxml.NodeType;
 
 import org.eclipse.core.resources.IFile;
@@ -21,13 +19,11 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.console.MessageConsoleStream;
-import org.eclipse.xtext.validation.Issue;
 import org.franca.connectors.dbus.DBusConnector;
 import org.franca.connectors.dbus.DBusModelContainer;
 import org.franca.core.dsl.FrancaPersistenceManager;
 import org.franca.core.dsl.ui.util.SpecificConsole;
 import org.franca.core.franca.FModel;
-import org.franca.core.utils.FrancaRecursiveValidator;
 
 import com.google.inject.Inject;
 
@@ -36,8 +32,7 @@ public class CreateFrancaFromDBusXMLAction implements IObjectActionDelegate {
 
 	private IStructuredSelection selection = null;
 	
-//	@Inject FrancaPersistenceManager loader;
-//	@Inject FrancaRecursiveValidator validator;
+	@Inject FrancaPersistenceManager saver;
 
 	@Override
 	public void run(IAction action) {
@@ -52,8 +47,7 @@ public class CreateFrancaFromDBusXMLAction implements IObjectActionDelegate {
             }
 
             IFile file = (IFile)selection.getFirstElement();
-//          String dbusFile = file.getLocationURI().toString();
-            String dbusFile = file.getFullPath().toString();
+            String dbusFile = file.getLocationURI().toString();
             String outputDir = file.getParent().getLocation().toString();
 
     		// load D-Bus Introspection XML file
@@ -65,57 +59,40 @@ public class CreateFrancaFromDBusXMLAction implements IObjectActionDelegate {
     			return;
     		}
     		NodeType node = dbus.model();
+    		if (node==null) {
+    			err.println("Error during load of D-Bus Introspection XML file '" + dbusFile + "'.");
+    			return;
+    		}
     		out.println("D-Bus Introspection XML: loaded node '" + node.getName() + "'");
     		
-//    		// validate resource
-//    		Collection<Issue> issues = validator.validate(fmodel.eResource());
-//    		int nErrors = 0;
-//    		for(Issue issue : issues) {
-//    			switch (issue.getSeverity()) {
-//    			case INFO:
-//    			case WARNING:
-//    				out.println(issue.toString());
-//    				break;
-//    			case ERROR:
-//    				err.println(issue.toString());
-//    				nErrors++;
-//    				break;
-//    			}
-//    		}
-//    		if (nErrors>0) {
-//    			err.println("Aborting due to validation errors!");
-//    			return;
-//    		}
-//  
-//    		// transform DBus Introspection XML to Franca
-//    		out.println("Transforming from DBus Introspection XML file ...");
-//    		DBusConnector dbusConn = new DBusConnector();
-//    		DBusModelContainer dbus = null;
-////    		try {
-//    			dbus = (DBusModelContainer) dbusConn.fromFranca(fmodel);
-////    		} catch (Exception e) {
-////    			err.println("Internal transformation error, aborting.");
-////				return;
-////    		}
-//    		
-//    		// save Franca fidl file
-//    		int ext = file.getName().lastIndexOf("." + file.getFileExtension());
-//    		String outfile = file.getName().substring(0, ext) + ".xml";
-//    		String outpath = outputDir + "/" + outfile;
-//    		if (dbusConn.saveModel(dbus, outpath)) {
-//    			out.println("Saved DBus Introspection file '" + outpath + "'.");
-//    		} else {
-//    			err.println("DBus Introspection file couldn't be written to file '" + outpath + "'.");
-//    		}
-//            
-//    		// refresh IDE (in order to make new files visible)
-//            IProject project = file.getProject();
-//            try {
-//    			project.refreshLocal(IResource.DEPTH_INFINITE, null);;
-//			} catch (CoreException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
+    		// transform DBus Introspection XML to Franca
+    		out.println("Transforming to Franca IDL model ...");
+    		FModel fmodel = null;
+    		try {
+    			fmodel = conn.toFranca(dbus);
+    		} catch (Exception e) {
+    			err.println("Internal transformation error, aborting.");
+				return;
+    		}
+    		
+    		// save Franca fidl file
+    		int ext = file.getName().lastIndexOf("." + file.getFileExtension());
+    		String outfile = file.getName().substring(0, ext) + ".fidl";
+    		String outpath = outputDir + "/" + outfile;
+    		if (saver.saveModel(fmodel, outpath)) {
+    			out.println("Saved Franca IDL file '" + outpath + "'.");
+    		} else {
+    			err.println("Franca IDL file couldn't be written to file '" + outpath + "'.");
+    		}
+            
+    		// refresh IDE (in order to make new files visible)
+            IProject project = file.getProject();
+            try {
+    			project.refreshLocal(IResource.DEPTH_INFINITE, null);;
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         }
 	}
 
