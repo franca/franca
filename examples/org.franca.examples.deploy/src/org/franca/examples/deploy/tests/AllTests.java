d6f9d43c1244508df467886dbf18f0c97d0f49c4
Klaus Birken <klaus.birken@gmail.com>
2013-04-16 21:43:51 +0200

    Added separate example project with deployment examples.

---
+++ examples/org.franca.examples.deploy/src/org/franca/examples/deploy/tests/AllTests.java
@@ -0,0 +1,15 @@
+package org.franca.examples.deploy.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	// generators
+	DeployGeneratorTest.class,
+	
+})
+
+
+public class AllTests {
+}
---
+++ examples/org.franca.examples.deploy/src/org/franca/examples/deploy/tests/TestConfiguration.java
@@ -0,0 +1,14 @@
+package org.franca.examples.deploy.tests;
+
+public class TestConfiguration {
+
+	public static String outputDir = "src-gen";
+	//public static String srcGenDir = "src-gen";
+
+	public static String fdeployArchFile =
+			"org/example/deploy/MediaPlayer_Arch.fdepl";
+
+	public static String fdeployInterfaceFile =
+			"org/example/deploy/MediaPlayer.fdepl";
+
+}
