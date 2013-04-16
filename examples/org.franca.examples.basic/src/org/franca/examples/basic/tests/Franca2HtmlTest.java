0fa9243a688c69549b9b0717acc73b188ead0bf0
Klaus Birken <klaus.birken@gmail.com>
2013-04-16 20:57:43 +0200

    Updated examples of basic-examples project. Updated JUnit test for Franca->html generation.

--- examples/org.franca.examples.basic/src/org/franca/examples/basic/tests/Franca2HtmlTest.java
+++ examples/org.franca.examples.basic/src/org/franca/examples/basic/tests/Franca2HtmlTest.java
@@ -1,8 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2013 itemis AG (http://www.itemis.de).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.franca.examples.basic.tests;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.xtext.junit4.InjectWith;
 import org.eclipse.xtext.junit4.XtextRunner;
 import org.franca.core.dsl.FrancaIDLInjectorProvider;
@@ -18,7 +26,6 @@ import com.google.inject.Inject;
  * Testcase for the Franca=>HTML transformation toolchain.
  * 
  * @author kbirken
- *
  */
 @RunWith(XtextRunner.class)
 @InjectWith(FrancaIDLInjectorProvider.class)
@@ -28,17 +35,35 @@ public class Franca2HtmlTest {
 	FrancaPersistenceManager loader;
 	
 	@Test
-	public void test() {
-		System.out.println("*** Franca2HtmlTest");
-		
-		// load example Franca IDL interface
-		String inputfile = "examples/" + TestConfiguration.francaFile; 
-		FModel fmodel = loader.loadModel(inputfile);
-		assertNotNull(fmodel);
-		System.out.println("Franca IDL: package '" + fmodel.getName() + "'");
-		
-		// create HTML documentation from Franca model
-		assertTrue(FrancaGenerators.instance().genHTML(fmodel, TestConfiguration.outputDir));
+	public void test01() {
+		genAndSave("org/example/MediaPlayer.fidl");
 	}
 
+	@Test
+	public void test02() {
+		genAndSave("org/example/BluetoothManager.fidl");
+	}
+
+	@Test
+	public void test03() {
+		genAndSave("org/example/PowerManagement.fidl");
+	}
+
+	@Test
+	public void test04() {
+		genAndSave("org/example/SystemdManager.fidl");
+	}
+
+
+	private void genAndSave (String filename) {
+		// load example Franca IDL interface
+    	URI root = URI.createURI("classpath:/");
+    	URI loc = URI.createFileURI(filename);
+    	FModel fmodel = loader.loadModel(loc, root);
+		assertNotNull(fmodel);
+		//System.out.println("Franca IDL: package '" + fmodel.getName() + "'");
+		
+		// create HTML documentation from Franca model
+		assertTrue(FrancaGenerators.instance().genHTML(fmodel, "html-gen"));
+	}
 }
