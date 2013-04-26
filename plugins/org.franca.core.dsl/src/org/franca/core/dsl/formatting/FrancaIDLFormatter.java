aeacb8e262048cc6b59503a612a9fc370ff328ff
szabta89 <szabta89@gmail.com>
2013-04-26 17:35:47 +0200

    fixed formatting for import elements

--- plugins/org.franca.core.dsl/src/org/franca/core/dsl/formatting/FrancaIDLFormatter.java
+++ plugins/org.franca.core.dsl/src/org/franca/core/dsl/formatting/FrancaIDLFormatter.java
@@ -49,6 +49,10 @@ public class FrancaIDLFormatter extends AbstractDeclarativeFormatter {
 		c.setLinewrap(0, 1, 2).before(f.getML_COMMENTRule());
 		c.setLinewrap(0, 1, 1).after(f.getML_COMMENTRule());
 		
+		//import in general
+		c.setLinewrap(1).before(f.getImportRule());
+		c.setLinewrap(1).after(f.getImportRule());
+		
 		// interface in general
 		c.setLinewrap(2).around(f.getFInterfaceRule());
 		
