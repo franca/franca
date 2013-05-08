09888d1bd3e93ce53476b193611286c83f58ce46
szabta89 <szabta89@gmail.com>
2013-05-08 14:40:38 +0200

    updated example validators #7

--- plugins/org.franca.core.validation.validators/src/org/franca/core/validation/validators/PackageNameValidator.java
+++ plugins/org.franca.core.validation.validators/src/org/franca/core/validation/validators/PackageNameValidator.java
@@ -1,79 +1,61 @@
 package org.franca.core.validation.validators;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.xtext.diagnostics.Severity;
+import org.eclipse.xtext.validation.ValidationMessageAcceptor;
+import org.franca.core.dsl.validation.IFrancaExternalValidator;
 import org.franca.core.franca.FModel;
 import org.franca.core.franca.FrancaPackage;
-import org.franca.core.validation.runtime.IFrancaValidator;
-import org.franca.core.validation.runtime.Issue;
 
-public class PackageNameValidator implements IFrancaValidator {
+public class PackageNameValidator implements IFrancaExternalValidator {
 
 	private static String francaEditorId = "org.franca.core.dsl.FrancaIDL";
 
 	private IWorkbenchWindow workbenchWindow;
 
 	@Override
-	public Collection<Issue> validateModel(Resource resource) {
-		FModel model = null;
-		for (EObject obj : resource.getContents()) {
-			if (obj instanceof FModel) {
-				model = (FModel) obj;
+	public void validateModel(FModel model,
+			ValidationMessageAcceptor messageAcceptor) {
+
+		Display.getDefault().syncExec(new Runnable() {
+			@Override
+			public void run() {
+				workbenchWindow = PlatformUI.getWorkbench()
+						.getActiveWorkbenchWindow();
 			}
-		}
+		});
 
-		if (model != null) {
-			Display.getDefault().syncExec(new Runnable() {
-				@Override
-				public void run() {
-					workbenchWindow = PlatformUI.getWorkbench()
-							.getActiveWorkbenchWindow();
-				}
-			});
+		if (workbenchWindow != null) {
+			IEditorPart editor = workbenchWindow.getActivePage()
+					.getActiveEditor();
 
-			List<Issue> issues = new ArrayList<Issue>();
+			if (editor.getEditorSite().getId().equals(francaEditorId)) {
+				IFile file = (IFile) editor.getEditorInput().getAdapter(
+						IFile.class);
+				IPath relativePath = file.getProjectRelativePath();
+				String[] tokens = relativePath.toString().split("/");
 
-			if (workbenchWindow != null) {
-				IEditorPart editor = workbenchWindow.getActivePage()
-						.getActiveEditor();
-
-				if (editor.getEditorSite().getId().equals(francaEditorId)) {
-					IFile file = (IFile) editor.getEditorInput().getAdapter(
-							IFile.class);
-					IPath relativePath = file.getProjectRelativePath();
-					String[] tokens = relativePath.toString().split("/");
-
-					StringBuilder sb = new StringBuilder();
-					if (tokens.length > 2) {
-						for (int i = 1; i < tokens.length - 1; i++) {
-							sb.append(tokens[i]);
-							if (i != tokens.length - 2) {
-								sb.append(".");
-							}
+				StringBuilder sb = new StringBuilder();
+				if (tokens.length > 2) {
+					for (int i = 1; i < tokens.length - 1; i++) {
+						sb.append(tokens[i]);
+						if (i != tokens.length - 2) {
+							sb.append(".");
 						}
 					}
+				}
 
-					if (!sb.toString().equals(model.getName())) {
-						issues.add(new Issue("The name of the container package and model package must be the same!", model, FrancaPackage.Literals.FMODEL__NAME, Severity.ERROR));
-					}
+				if (!sb.toString().equals(model.getName())) {
+					messageAcceptor.acceptError(
+							"The name of the container package and model package must be the same!",
+							model, FrancaPackage.Literals.FMODEL__NAME, ValidationMessageAcceptor.INSIGNIFICANT_INDEX, null);
 				}
 			}
-
-			return issues;
 		}
-		return Collections.emptyList();
 	}
 
 }
--- plugins/org.franca.core.validation.validators/src/org/franca/core/validation/validators/UniqueStateNameValidator.java
+++ plugins/org.franca.core.validation.validators/src/org/franca/core/validation/validators/UniqueStateNameValidator.java
@@ -1,27 +1,21 @@
 package org.franca.core.validation.validators;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.xtext.diagnostics.Severity;
 import org.eclipse.xtext.resource.DefaultLocationInFileProvider;
 import org.eclipse.xtext.resource.ILocationInFileProvider;
+import org.eclipse.xtext.validation.ValidationMessageAcceptor;
+import org.franca.core.dsl.validation.IFrancaExternalValidator;
 import org.franca.core.franca.FContract;
 import org.franca.core.franca.FInterface;
 import org.franca.core.franca.FModel;
 import org.franca.core.franca.FState;
 import org.franca.core.franca.FrancaPackage;
-import org.franca.core.validation.runtime.IFrancaValidator;
-import org.franca.core.validation.runtime.Issue;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 
-public class UniqueStateNameValidator implements IFrancaValidator {
+public class UniqueStateNameValidator implements IFrancaExternalValidator {
 
 	ILocationInFileProvider locationProvider;
 
@@ -30,37 +24,29 @@ public class UniqueStateNameValidator implements IFrancaValidator {
 	}
 
 	@Override
-	public Collection<Issue> validateModel(Resource resource) {
-		FModel model = null;
-		for (EObject obj : resource.getContents()) {
-			if (obj instanceof FModel) {
-				model = (FModel) obj;
+	public void validateModel(FModel model,
+			ValidationMessageAcceptor messageAcceptor) {
+
+		for (FInterface _interface : model.getInterfaces()) {
+			FContract contract = _interface.getContract();
+			Multimap<String, FState> stateNameMap = ArrayListMultimap.create();
+
+			for (FState state : contract.getStateGraph().getStates()) {
+				stateNameMap.put(state.getName(), state);
 			}
-		}
 
-		if (model != null) {
-			List<Issue> issues = new ArrayList<Issue>();
-			for (FInterface _interface : model.getInterfaces()) {
-				FContract contract = _interface.getContract();
-				Multimap<String, FState> stateNameMap = ArrayListMultimap.create();
-
-				for (FState state : contract.getStateGraph().getStates()) {
-					stateNameMap.put(state.getName(), state);
-				}
-				
-				for (String name : stateNameMap.keySet()) {
-					Collection<FState> states = stateNameMap.get(name);
-					if (states.size() > 1) {
-						for (FState state : states) {
-							issues.add(new Issue("The name of the state is not unique!", state, FrancaPackage.Literals.FMODEL_ELEMENT__NAME, Severity.ERROR));
-						}
+			for (String name : stateNameMap.keySet()) {
+				Collection<FState> states = stateNameMap.get(name);
+				if (states.size() > 1) {
+					for (FState state : states) {
+						messageAcceptor.acceptError(
+								"The name of the state is not unique!", state,
+								FrancaPackage.Literals.FMODEL_ELEMENT__NAME,
+								ValidationMessageAcceptor.INSIGNIFICANT_INDEX, 
+								null);
 					}
 				}
 			}
-
-			return issues;
 		}
-		return Collections.emptyList();
 	}
-
 }
