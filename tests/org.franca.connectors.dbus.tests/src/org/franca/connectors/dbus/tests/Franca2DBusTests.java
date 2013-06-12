e469647f39229ac00751b6b87aa423fb5426c777
Klaus Birken <klaus.birken@gmail.com>
2013-06-12 16:56:22 +0200

    Added some unit tests for the D-Bus connector.

---
+++ tests/org.franca.connectors.dbus.tests/src/org/franca/connectors/dbus/tests/Franca2DBusTests.java
@@ -0,0 +1,78 @@
+package org.franca.connectors.dbus.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.eclipse.emf.compare.Comparison;
+import org.eclipse.emf.compare.Diff;
+import org.eclipse.emf.compare.EMFCompare;
+import org.eclipse.emf.compare.internal.spec.ResourceAttachmentChangeSpec;
+import org.eclipse.emf.compare.scope.IComparisonScope;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipselabs.xtext.utils.unittesting.XtextRunner2;
+import org.franca.connectors.dbus.DBusConnector;
+import org.franca.connectors.dbus.DBusModelContainer;
+import org.franca.core.dsl.FrancaIDLTestsInjectorProvider;
+import org.franca.core.dsl.FrancaPersistenceManager;
+import org.franca.core.franca.FModel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.google.inject.Inject;
+
+@RunWith(XtextRunner2.class)
+@InjectWith(FrancaIDLTestsInjectorProvider.class)
+public class Franca2DBusTests {
+
+	@Inject	FrancaPersistenceManager loader;
+	
+	@Test
+	public void test_30() {
+		doTransformTest("30-SimpleAttribute");
+	}
+	
+	@Test
+	public void test_40() {
+		doTransformTest("40-PolymorphicStructs");
+	}
+
+
+	@SuppressWarnings("restriction")
+	private void doTransformTest (String fileBasename) {
+		// load example Franca IDL interface
+		String inputfile = "model/testcases/" + fileBasename + ".fidl";
+		System.out.println("Loading Franca file " + inputfile + " ...");
+		FModel fmodel = loader.loadModel(inputfile);
+		assertNotNull(fmodel);
+		
+		// transform to D-Bus Introspection XML
+		DBusConnector conn = new DBusConnector();
+		DBusModelContainer fromFranca = (DBusModelContainer)conn.fromFranca(fmodel);
+		conn.saveModel(fromFranca, "src-gen/testcases/" + fileBasename + ".xml");
+		
+		// load reference D-Bus xml file
+		String referenceFile = "model/reference/" + fileBasename + ".xml";
+		DBusModelContainer ref = (DBusModelContainer)conn.loadModel(referenceFile);
+		
+		// compare with reference file
+		ResourceSet rset1 = fromFranca.model().eResource().getResourceSet();
+		ResourceSet rset2 = ref.model().eResource().getResourceSet();
+
+		IComparisonScope scope = EMFCompare.createDefaultScope(rset1, rset2);
+		Comparison comparison = EMFCompare.builder().build().compare(scope);
+		 
+		List<Diff> differences = comparison.getDifferences();
+		int nDiffs = 0;
+		for (Diff diff : differences) {
+			if (! (diff instanceof ResourceAttachmentChangeSpec)) {
+				System.out.println(diff.toString());
+				nDiffs++;
+			}
+		}
+		assertEquals(nDiffs, 0);
+	}
+}
+
