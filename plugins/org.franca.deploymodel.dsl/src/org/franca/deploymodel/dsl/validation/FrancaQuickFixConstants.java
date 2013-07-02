291a4d8479f765fe14db109b1e84741ce7824c14
Tamas Szabo <tamas.szabo@itemis.de>
2013-07-02 11:35:32 +0200

    added recursive quick fix for deployment interfaces #38

--- plugins/org.franca.deploymodel.dsl/src/org/franca/deploymodel/dsl/validation/FrancaQuickFixConstants.java
+++ plugins/org.franca.deploymodel.dsl/src/org/franca/deploymodel/dsl/validation/FrancaQuickFixConstants.java
@@ -1,7 +1,16 @@
 package org.franca.deploymodel.dsl.validation;
 
+import org.franca.deploymodel.dsl.fDeploy.FDElement;
+
+/**
+ * These constants indicated {@link FDElement} types during quickfix resolution. 
+ * 
+ * @author Tamas Szabo
+ *
+ */
 public enum FrancaQuickFixConstants {
 
+	INTERFACE,
 	ATTRIBUTE,
 	METHOD,
 	BROADCAST,
