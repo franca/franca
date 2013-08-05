fa47ced5c882921a3e68f01eee0ca828e1f9a52e
Steffen Weik <steffen.weik@itemis.de>
2013-08-05 16:25:03 +0200

    removing some files that are not necessary anymore

--- plugins/org.franca.tools.contracts.tracegen/xtend-gen/org/franca/tools/contracts/tracegen/strategies/selectors/TransitionSelector.java
+++
@@ -1,9 +0,0 @@
-package org.franca.tools.contracts.tracegen.strategies.selectors;
-
-import org.franca.core.franca.FTransition;
-import org.franca.tools.contracts.tracegen.traces.Trace;
-
-@SuppressWarnings("all")
-public interface TransitionSelector {
-  public abstract FTransition execute(final Trace currentTrace, final Iterable<FTransition> possibilities);
-}
