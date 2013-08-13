1af53208b852977c65530c571db749a96555b331
Tamas Szabo <tamas.szabo@itemis.de>
2013-08-13 21:53:04 +0200

    added trace validator plugin to experimental branch

---
+++ plugins/org.franca.tools.contracts.validator/src/org/franca/tools/contracts/validator/parser/ITraceParser.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2012 itemis AG (http://www.itemis.de).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.franca.tools.contracts.validator.parser;
+
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.franca.core.franca.FEventOnIf;
+import org.franca.core.franca.FModel;
+
+public abstract class ITraceParser {
+
+	protected FModel model;
+	
+	public ITraceParser(FModel model) {
+		this.model = model;
+	}
+	
+	public abstract List<FEventOnIf> parseTrace(IFile file);
+	
+}
