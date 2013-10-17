fab524830a0f05190d6bf28bcdf25537c1f657e0
Klaus Birken <klaus.birken@gmail.com>
2013-10-17 18:48:31 +0200

    Extracted websocket stuff from basic example project into separate one.

--- examples/org.franca.examples.websockets/src/org/franca/examples/websockets/tests/WebsocketGenTest.java
+++ examples/org.franca.examples.websockets/src/org/franca/examples/websockets/tests/WebsocketGenTest.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.franca.examples.basic.tests;
+package org.franca.examples.websockets.tests;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
