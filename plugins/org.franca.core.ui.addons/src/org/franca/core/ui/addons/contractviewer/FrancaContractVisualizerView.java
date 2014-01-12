497d365559f7fee96ce1835ea729bd0b60afe30f
Tamas Szabo <tamas.szabo@itemis.de>
2014-01-12 20:45:06 +0100

    removed unused imports

--- plugins/org.franca.core.ui.addons/src/org/franca/core/ui/addons/contractviewer/FrancaContractVisualizerView.java
+++ plugins/org.franca.core.ui.addons/src/org/franca/core/ui/addons/contractviewer/FrancaContractVisualizerView.java
@@ -12,9 +12,9 @@ import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gef4.layout.algorithms.SpringLayoutAlgorithm;
 import org.eclipse.gef4.zest.core.widgets.Graph;
 import org.eclipse.gef4.zest.core.widgets.ZestStyles;
-import org.eclipse.gef4.layout.algorithms.SpringLayoutAlgorithm;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Composite;
@@ -33,7 +33,6 @@ import org.franca.core.ui.addons.contractviewer.util.FrancaEditorPartListener;
 import org.franca.core.ui.addons.contractviewer.util.GraphSelectionListener;
 import org.franca.core.ui.addons.contractviewer.util.IntermediateFrancaGraphModel;
 import org.franca.core.ui.addons.contractviewer.util.ResourceChangeListener;
-import org.franca.core.utils.FrancaRecursiveValidator;
 
 import com.google.inject.Inject;
 import com.google.inject.Injector;
@@ -63,10 +62,7 @@ public class FrancaContractVisualizerView extends ViewPart {
 	
 	@Inject
 	private Injector injector;
-	
-	@Inject
-	private FrancaRecursiveValidator validator;
-	
+
 	public FrancaContractVisualizerView() {
 		this.previousIntermediateModel = null;
 		this.displayLabel = false;
