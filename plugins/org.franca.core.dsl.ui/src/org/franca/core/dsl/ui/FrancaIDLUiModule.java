dbe4051245a75ee5a7f6d2e7c598a8a39f2fd7cc
Tamas Szabo <tamas.szabo@itemis.de>
2014-02-25 13:51:14 +0100

    added JDT-free Xtext bindings
    + conflict resolution.

--- plugins/org.franca.core.dsl.ui/src/org/franca/core/dsl/ui/FrancaIDLUiModule.java
+++ plugins/org.franca.core.dsl.ui/src/org/franca/core/dsl/ui/FrancaIDLUiModule.java
@@ -4,10 +4,18 @@
 package org.franca.core.dsl.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.resource.containers.IAllContainersState;
+import org.eclipse.xtext.ui.editor.model.IResourceForEditorInputFactory;
+import org.eclipse.xtext.ui.editor.model.ResourceForIEditorInputFactory;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
+import org.eclipse.xtext.ui.resource.IResourceSetProvider;
+import org.eclipse.xtext.ui.resource.SimpleResourceSetProvider;
+import org.eclipse.xtext.ui.shared.Access;
 import org.franca.core.dsl.ui.highlighting.FrancaHighlightingConfiguration;
 import org.franca.core.dsl.ui.highlighting.FrancaSemanticHighlightingCalculator;
+
+import com.google.inject.Provider;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -26,5 +34,20 @@ public class FrancaIDLUiModule extends
 	// inject own semantic highlighting
 	public Class<? extends ISemanticHighlightingCalculator> bindSemanticHighlightingCalculator() {
 		return FrancaSemanticHighlightingCalculator.class;
+	}
+	
+	@Override
+	public Provider<IAllContainersState> provideIAllContainersState() {
+		return Access.getWorkspaceProjectsState();
+	}
+
+	@Override
+	public Class<? extends IResourceSetProvider> bindIResourceSetProvider() {
+		return SimpleResourceSetProvider.class;
+	}
+
+	@Override
+	public Class<? extends IResourceForEditorInputFactory> bindIResourceForEditorInputFactory() {
+		return ResourceForIEditorInputFactory.class;
 	}
 }
