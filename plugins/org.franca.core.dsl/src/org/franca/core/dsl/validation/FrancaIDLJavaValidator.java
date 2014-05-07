f0fb913755d1b2b484797d53b815c6eeb8d9bd7b
Klaus Birken <klaus.birken@gmail.com>
2014-05-07 15:12:19 +0200

    Validate range specification for integer intervals.

--- plugins/org.franca.core.dsl/src/org/franca/core/dsl/validation/FrancaIDLJavaValidator.java
+++ plugins/org.franca.core.dsl/src/org/franca/core/dsl/validation/FrancaIDLJavaValidator.java
@@ -40,6 +40,7 @@ import org.franca.core.franca.FDeclaration;
 import org.franca.core.franca.FEnumerationType;
 import org.franca.core.franca.FEnumerator;
 import org.franca.core.franca.FGuard;
+import org.franca.core.franca.FIntegerInterval;
 import org.franca.core.franca.FInterface;
 import org.franca.core.franca.FMethod;
 import org.franca.core.franca.FModel;
@@ -362,6 +363,19 @@ public class FrancaIDLJavaValidator extends AbstractFrancaIDLJavaValidator
 	public void checkEnumValue (FEnumerator enumerator) {
 		if (enumerator.getValue() != null)
 			TypesValidator.checkEnumValueType(this, enumerator);
+	}
+
+	@Check
+	public void checkIntegerInterval (FTypeRef intervalType) {
+		if (intervalType.getInterval()!=null) {
+			FIntegerInterval interval = intervalType.getInterval();
+			if (interval.getLowerBound()!=null && interval.getUpperBound()!=null) {
+				if (interval.getLowerBound().compareTo(interval.getUpperBound()) > 0) {
+					error("Invalid interval specification", intervalType,
+							FrancaPackage.Literals.FTYPE_REF__INTERVAL, -1);
+				}
+			}
+		}
 	}
 
 	
