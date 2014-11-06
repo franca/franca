930a26036691ceaa519038b1da3833dac59fde37
Vinay <Vinay@172.16.2.201>
2014-11-06 11:13:30 +0100

    Added value converters for float and double

--- plugins/org.franca.core.dsl/src/org/franca/core/dsl/valueconverter/FrancaValueConverters.java
+++ plugins/org.franca.core.dsl/src/org/franca/core/dsl/valueconverter/FrancaValueConverters.java
@@ -14,7 +14,42 @@ import org.eclipse.xtext.conversion.impl.AbstractNullSafeConverter;
 import org.eclipse.xtext.nodemodel.INode;
 
 public class FrancaValueConverters extends Ecore2XtextTerminalConverters {
+	@ValueConverter(rule = "E_FLOAT_OBJECT")
+	public AbstractNullSafeConverter<Float> E_FLOAT_OBJECT() {
+		return new AbstractNullSafeConverter<Float>() {
+			@Override
+			protected Float internalToValue(String string, INode node) {
+				Float value = Float.parseFloat(string);
+				
+				
+				
+				return value;
+			}
 
+			@Override
+			protected String internalToString(Float value) {
+				return value.toString();
+			}
+		};
+	}
+	@ValueConverter(rule = "E_DOUBLE_OBJECT")
+	public AbstractNullSafeConverter<Double> E_DOUBLE_OBJECT() {
+		return new AbstractNullSafeConverter<Double>() {
+			@Override
+			protected Double internalToValue(String string, INode node) {
+				Double value = Double.parseDouble(string);
+				
+				
+				
+				return value;
+			}
+
+			@Override
+			protected String internalToString(Double value) {
+				return value.toString();
+			}
+		};
+	}
 	@ValueConverter(rule = "ANNOTATION_STRING")
 	public IValueConverter<String> ANNOTATION_STRING() {
 		return new AbstractNullSafeConverter<String>() {
@@ -40,4 +75,6 @@ public class FrancaValueConverters extends Ecore2XtextTerminalConverters {
 			}
 		};
 	}
+	
+	
 }
