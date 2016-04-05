fc791853c7c13b36e83c8fd4a81e008a62f7f7ea
Klaus Birken <klaus.birken@gmail.com>
2016-04-05 17:12:57 +0200

    Added gitignore, removed generated files from repo.

--- tests/org.franca.core.dsl.tests.base/xtend-gen/org/franca/core/dsl/tests/compare/IComparisonReportGenerator.java
+++
@@ -1,8 +0,0 @@
-package org.franca.core.dsl.tests.compare;
-
-import org.eclipse.emf.compare.Match;
-
-@SuppressWarnings("all")
-public interface IComparisonReportGenerator {
-  public abstract String generateReport(final Match m);
-}
