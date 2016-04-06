8c077f565f0ffb050ee7b8ff24ac6b7f23992615
Klaus Birken <klaus.birken@gmail.com>
2016-04-06 17:41:15 +0200

    Refactoring: Extracted framework classes for building Franca-related command line tools.

--- plugins/org.franca.connectors.idl/src/org/franca/connectors/idl/Franca2IdlStandalone.java
+++ plugins/org.franca.connectors.idl/src/org/franca/connectors/idl/Franca2IdlStandalone.java
@@ -4,103 +4,67 @@ import java.io.File;
 import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.log4j.Logger;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.diagnostics.Severity;
 import org.eclipse.xtext.validation.CheckMode;
-import org.eclipse.xtext.validation.IResourceValidator;
 import org.eclipse.xtext.validation.Issue;
-import org.franca.core.dsl.FrancaIDLStandaloneSetup;
 import org.franca.core.dsl.FrancaIDLVersion;
-import org.franca.core.dsl.FrancaPersistenceManager;
+import org.franca.core.dsl.cli.AbstractCommandLineTool;
+import org.franca.core.dsl.cli.CommonOptions;
 import org.franca.core.franca.FModel;
 import org.franca.core.utils.FileHelper;
 
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 
 /**
  * Runner for standalone Franca2Idl transformation from Franca files.
  *  
  * @author Klaus Birken (itemis)
  */
-public class Franca2IdlStandalone {
+public class Franca2IdlStandalone extends AbstractCommandLineTool {
 
 	// prepare class for logging....
 	private static final Logger logger = Logger.getLogger(Franca2IdlStandalone.class);
 
-	private static final String TOOL_VERSION = "0.1.0";
-	private static final String FIDL_VERSION = 
-			FrancaIDLVersion.getMajor() + "." + FrancaIDLVersion.getMinor();
-	
-	private static final String HELP = "h";
+	private static final String TOOL_VERSION = "0.2.0";
+
+	// specific option values
 	private static final String FIDLFILE = "f";
-	private static final String OUTDIR = "o";
 	private static final String RECURSIVE_VALIDATION = "r";
 	
-	private static final String VERSIONSTR =
-			"Franca2IdlStandalone " + TOOL_VERSION + " (Franca IDL version " + FIDL_VERSION + ").";
-	
-	private static final String HELPSTR =
-			"java -jar Franca2IdlStandalone.jar [OPTIONS]";
-
-	private static Injector injector;
-	
 	/**
-	 * @param args
+	 * The main function for this standalone tool.</p>
+	 * 
+	 * It directly hands over control to the CommandLineTool framework.
+	 * 
+	 * @param args the collection of command line arguments
 	 */
-	public static void main(String[] args) throws Exception {
-		injector = new FrancaIDLStandaloneSetup().createInjectorAndDoEMFRegistration();
-		int retval = injector.getInstance(Franca2IdlStandalone.class).run(args);
-		if (retval != 0)
-			System.exit(retval);
+	public static void main(String[] args) {
+		// hand over control to CommandLineTool framework
+		execute(TOOL_VERSION, Franca2IdlStandalone.class, args);
 	}
 
 
 	// injected fragments
-	@Inject	FrancaPersistenceManager fidlLoader;
-
-	@Inject IResourceValidator validator;
 	@Inject	Franca2IdlConverter generator;
 
-	public int run(String[] args) throws Exception {
-		Options options = getOptions();
-
+	@Override
+	protected int run(CommandLine line) {
 		// print version string
 //		System.out.println(VERSIONSTR);
-		
-		// create the parser
-		CommandLineParser parser = new GnuParser();
-		CommandLine line = null;
-		final HelpFormatter formatter = new HelpFormatter();
-		try {
-			line = parser.parse(options, args);
-		} catch (final ParseException exp) {
-			logger.error(exp.getMessage());
-			formatter.printHelp(HELPSTR, options);
-			return -1;
-		}
-
-		if (line.hasOption(HELP) || checkCommandLineValues(line) == false) {
-			formatter.printHelp(HELPSTR, options);
-			return -1;
-		}
 
 		// TODO configure Logger depends on given verbose value
 //		boolean verbose = line.hasOption(VERBOSE);
 		
 		// load Franca IDL file
 		String fidlFile = line.getOptionValue(FIDLFILE);
-		FModel fmodel = fidlLoader.loadModel(fidlFile);
+		FModel fmodel = persistenceManager.loadModel(fidlFile);
 		if (fmodel==null) {
 			logger.error("Couldn't load Franca IDL file '" + fidlFile + "'.");
 			return -1;
@@ -136,8 +100,8 @@ public class Franca2IdlStandalone {
 		
 		// call generator and save files
 		String output = generator.generateAll(fmodel).toString();
-		if (line.hasOption(OUTDIR)) {
-			String outputFolder = line.getOptionValue(OUTDIR);
+		if (line.hasOption(CommonOptions.OUTDIR)) {
+			String outputFolder = line.getOptionValue(CommonOptions.OUTDIR);
 			String outPath = outputFolder + "/" + fmodel.getName().replaceAll("[.]", "/");
 			String filename = getBasename(mainResource.getURI()) + ".idl";
 			FileHelper.save(outPath, filename, output);
@@ -151,19 +115,9 @@ public class Franca2IdlStandalone {
 	}
 	
 	@SuppressWarnings("static-access")
-	private Options getOptions() {
+	@Override
+	protected void addOptions(Options options) {
 		// String[] set = LogFactory.getLog(getClass()).
-		final Options options = new Options();
-
-		// optional
-//		Option optVerbose = OptionBuilder.withArgName("verbose")
-//				.withDescription("Print Out Verbose Information").hasArg(false)
-//				.isRequired(false).create(VERBOSE);
-//		options.addOption(optVerbose);
-		Option optHelp = OptionBuilder.withArgName("help").withDescription("Print Usage Information")
-				.hasArg(false).isRequired(false)
-				.create(HELP);
-		options.addOption(optHelp);
 
 		// required
 		Option optInputFidl = OptionBuilder.withArgName("Franca IDL file")
@@ -172,22 +126,27 @@ public class Franca2IdlStandalone {
 		//optInputFidl.setType(File.class);
 		options.addOption(optInputFidl);
 
-		// optional
-		Option optOutputDir = OptionBuilder.withArgName("output directory")
-				.withDescription("Directory where the generated files will be stored")
-				.hasArg().withValueSeparator(' ').create(OUTDIR);
-		options.addOption(optOutputDir);
+		// option for configuration of an output directory 
+		CommonOptions.createOutdirOption(options);
 
 		Option optRecursiveValidation = OptionBuilder.withArgName("recval")
 		.withDescription("Recursive validation").hasArg(false)
 		.isRequired(false).create(RECURSIVE_VALIDATION);
 		options.addOption(optRecursiveValidation);
-
-		return options;
 	}
 
+	@Override
+	protected void logError(String message) {
+		logger.error(message);
+	}
 	
-	private boolean checkCommandLineValues(CommandLine line) {
+	@Override
+	protected void logInfo(String message) {
+		logger.info(message);
+	}
+	
+	@Override
+	protected boolean checkCommandLineValues(CommandLine line) {
 		if (line.hasOption(FIDLFILE)) {
 			String fidlFile = line.getOptionValue(FIDLFILE);
 			File fidl = new File(fidlFile);
