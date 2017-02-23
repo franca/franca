3c7eb60421b1ab032e0377e9b6abf66a3fed2e6b
Klaus Birken <klaus.birken@gmail.com>
2017-02-23 12:23:11 +0100

    Provide infrastructure for routing Franca connector output to log4j logger.

--- plugins/org.franca.core/src/org/franca/core/framework/AbstractFrancaConnector.xtend
+++ plugins/org.franca.core/src/org/franca/core/framework/AbstractFrancaConnector.xtend
@@ -1,6 +1,8 @@
 package org.franca.core.framework
 
 import java.io.PrintStream
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
 
 abstract class AbstractFrancaConnector implements IFrancaConnector {
 
@@ -12,4 +14,9 @@ abstract class AbstractFrancaConnector implements IFrancaConnector {
 		this.err = err
 	}
 	
+	override void setLogger(Logger logger) {
+		this.out = new PrintStream(new LoggingOutputStream(logger, Level.INFO), false)
+		this.err = new PrintStream(new LoggingOutputStream(logger, Level.ERROR), false)
+	}
+
 }
---
+++ plugins/org.franca.core/src/org/franca/core/framework/LoggingOutputStream.java
@@ -0,0 +1,67 @@
+package org.franca.core.framework;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+/*
+ * An OutputStream which logs to a log4j logger.
+ */
+public class LoggingOutputStream extends OutputStream {
+
+	/** The target logger. */
+    private Logger targetLogger;
+
+    /** Current log level (will be used for subsequent flush() calls). */
+    private Level currentLogLevel;
+
+    /**
+     * Constructor.
+     *
+     * @param logger the target logger
+     * @param level the initial log level
+     */
+    public LoggingOutputStream(Logger logger, Level level) {
+        this.targetLogger = logger;
+        this.currentLogLevel = level;
+    }
+
+    /**
+     * Change current logging level.
+     */
+    public void setLevel(Level level) {
+        this.currentLogLevel = level;
+    }
+
+    /** Buffer which is written on next flush() call. */
+    private String buffer = "";
+
+    /**
+     * Accumulate bytes for writing them to the logger.
+     */
+    @Override
+    public void write(int b) throws IOException {
+    	// convert integer to byte to character
+        byte[] singleByte = new byte[1];
+        singleByte[0] = (byte) (b & 0xff);
+        String c = new String(singleByte);
+        
+        if (c.equals("\n")) {
+            // automatically flush if end-of-line is detected
+            flush();
+        } else {
+        	buffer += c;
+        }
+    }
+
+    /**
+     * Ensure that current buffer is written to logger.
+     */
+    @Override
+    public void flush() {
+        targetLogger.log(currentLogLevel, buffer);
+        buffer = "";
+    }
+}
